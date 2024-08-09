b2f3a937e7dac00d40617e81912debdf2c5c3b3f
rubenyw <rubenyasonwinarta@gmail.com>
2024-08-09 19:39:23 +0700

    update

--- training04/resources/views/layout.blade.php
+++ training04/resources/views/layout.blade.php
@@ -10,3 +10,31 @@
     
 </body>
 </html>
+
+<script>
+    function formatRupiah(angka, prefix) {
+    angka = angka.toString();
+    var number_string = angka.replace(/[^,\d]/g, "").toString(),
+        split = number_string.split(","),
+        sisa = split[0].length % 3,
+        rupiah = split[0].substr(0, sisa),
+        ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+    if (ribuan) {
+        separator = sisa ? "." : "";
+        rupiah += separator + ribuan.join(".");
+    }
+
+    rupiah = split[1] != undefined ? rupiah + "," + split[1] : rupiah;
+    return prefix == undefined ? rupiah : rupiah ? prefix + rupiah : "";
+}
+
+    function convertToAngka(rupiah) {
+        return parseInt(rupiah.replace(/,.*|[^0-9]/g, ""), 10);
+    }
+
+    function getCurrentDate() {
+        var local = new Date();
+        local.setMinutes(local.getMinutes() - local.getTimezoneOffset());
+        return local.toJSON().slice(0, 10);
+    }
+</script>
---
+++ training04/resources/views/transaksi.blade.php
@@ -0,0 +1,59 @@
+@section('body')
+    <div class="row">
+        <input type="text" name="transaksi_date" id="transaksi_date">
+    </div>
+@endsection
+
+@section('custom-js')
+<script>
+    var barang = @json($barang);
+    var transaksi = [];
+
+    $(document).on("click", "#btn-tambah", function(){
+        var index = $('#barang').val();
+        var data = barang[index];
+        var ada = -1;
+
+        transaksi.forEach((item, index) => {
+            if(item.arang.barang_id == data.barang_id){
+                transaksi[index].jumlah += parseInt($('#jumlah').val());
+                transaksi[index].subtotal = $('#jumlah').val() * data.barang_harga;
+                ada = 1;
+            }
+        })
+
+        var temp = {
+            barang: data,
+            jumlah: $("#jumlah").val()
+        };
+        transaksi.push(temp);
+    })
+
+    function refreshData() {
+        var total = 0;
+        $("#tb-transaksi").html("");
+
+        transaksi.forEach(element => {
+            $(`#tb-transaksi`).append(`
+                <tr>
+                    <td>
+                        ${item.barang.barang_code}
+                    </td>
+                    <td>
+                        ${item.barang_name}
+                    </td>
+                    <td>
+                        ${item.total}
+                    </td>
+                    <td>
+                        ${item.barang.barang_price}
+                    </td>
+                    <td>
+                        ${number_format}
+                    </td>
+                </tr>
+            `);
+        });
+    }
+</script>
+@endsection
